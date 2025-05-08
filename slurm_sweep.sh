459e53fa44a79158d2ae0724c1aedf1a08089688
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 17:21:35 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 start_sweep_job=start_sweep_job.sh
+sweep_id_file=sweep_id.txt
 sbatch $start_sweep_job
-sweep_id=$(< sweep_id.txt)
+sweep_id=$(< "$sweep_id_file")
 echo "$sweep_id"
 jobfile=slurm_pass_forward.sh
 
