b8323492777d8347de360be5a93b6632e174ed27
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 16:12:29 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 sweep_id=$(python start_sweep.py --entity=snn_nlp --project=lyapunov_snn --config=sweep.yaml)
 echo "$sweep_id"
+echo "*************"
+echo "$sweep_id"
+echo "*************"
 jobfile=slurm_pass_forward.sh
 
 for ((i=1; i<=$1; i++)); do
-    sbatch $jobfile $sweep_id
+    sbatch $jobfile "$sweep_id"
     sleep 30
     echo "waiting"
 done
