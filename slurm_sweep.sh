7a65a983159f0a5fa1798a7e5a62be64737183f7
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 01:11:09 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -8,7 +8,6 @@ for ((i=1; i<=$2; i++)); do
     echo "#!/bin/sh" >> $jobfile
     echo "#SBATCH --job-name=d3" >> $jobfile
     echo "#SBATCH -c 1" >> $jobfile
-    echo "#SBATCH --nodelist=ax19" >> $jobfile
     echo "#SBATCH --time=24:00:00" >> $jobfile
     echo "#SBATCH --mem-per-cpu=2gb" >> $jobfile
     echo "#SBATCH --cpus-per-task=1" >>$jobfile
