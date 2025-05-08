1d7a6a587b19306be25b8b23a7a069d1ccae8355
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 01:41:39 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -9,8 +9,8 @@ for ((i=1; i<=$2; i++)); do
     echo "#SBATCH --job-name=d3" >> $jobfile
     echo "#SBATCH -c 1" >> $jobfile
     echo "#SBATCH --time=8:00:00" >> $jobfile
-    echo "#SBATCH --mem-per-cpu=2gb" >> $jobfile
-    echo "#SBATCH --cpus-per-task=1" >>$jobfile
+    echo "#SBATCH --mem-per-cpu=4gb" >> $jobfile
+    echo "#SBATCH --cpus-per-task=3" >>$jobfile
     echo "wandb agent $1" >>$jobfile
     echo "date" >> $jobfile
     
