7dbaecfaf7e48811ca025d50f058a704c4a67a8c
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 15:10:12 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sweep_id = $(python start_sweep.py --entity=snn_nlp --project=lyapunov_snn --config=sweep.yaml)
-echo $sweep_id
+echo "$sweep_id"
 jobfile=slurm_pass_forward.sh
 
 for ((i=1; i<=$1; i++)); do
