fb2b6fb8860d0ac5e58b16dfae4b612680ee043c
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 00:15:23 -0400

    wandb sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -4,7 +4,7 @@ SWEEP_ID = "snn_nlp/lyapunov_snn/m4poin1v"
 
 wandb sweep --update $SWEEP_ID sweep.yaml
 
-for i in {1..2}; do 
+for ((i = 1; i <= $2; i++ )); do
     jobfile="sgf_job"
     
     echo "#!/bin/sh" >> $jobfile
@@ -13,7 +13,7 @@ for i in {1..2}; do
     echo "#SBATCH --time=24:00:00" >> $jobfile
     echo "#SBATCH --mem-per-cpu=2gb" >> $jobfile
     echo "#SBATCH --cpus-per-task=1" >>$jobfile
-    echo "wandb agent --count $NUM $SWEEP_ID" >>$jobfile
+    echo "wandb agent --count $1 $SWEEP_ID" >>$jobfile
     echo "date" >> $jobfile
     
     sbatch $jobfile
