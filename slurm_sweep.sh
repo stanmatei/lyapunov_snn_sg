174286bc4e59a1243e89577760960006cac9a481
f29919ms <f29919ms@C-LOSXPH3FH.local>
2025-05-08 14:48:00 -0400

    sweep changes

--- slurm_sweep.sh
+++ slurm_sweep.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sweep_id=$(wandb sweep --entity=snn_nlp --project=lyapunov_snn sweep.yaml | awk '/ID:/{print $2}')
+sweep_id=$(wandb sweep --entity=snn_nlp --project=lyapunov_snn sweep.yaml)
 echo $sweep_id
+sweep_out=${}
 jobfile=slurm_pass_forward.sh
 
 for ((i=1; i<=$1; i++)); do
